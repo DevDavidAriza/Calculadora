1f59478f10495eafc20b485bd71555a5560e1316
David Ariza <david.ariza@pi.edu.co>
2021-02-16 19:55:29 -0500

    se agregaron funciones

--- Calculadora.py
+++ Calculadora.py
@@ -1,18 +1,36 @@
-#se agrega un ciclo para repetir la pregunta
+#se divide en funciones para mejorar la carga del programa
+
+def sumar(x,y):
+    respuesta = x + y
+    return respuesta
+
+def restar(x,y):
+    respuesta = x - y
+    return respuesta
+
+def multiplicar(x,y):
+    respuesta = x * y
+    return respuesta
+
+def dividir(x,y):
+    respuesta = x / y
+    return respuesta
+
 
 repetir = "si"
 while repetir.lower() == "si":
     n1 = float(input("deme un numero\n"))
     n2 = float(input("deme otro numero\n"))
     operacion = str(input("que operacion desea hacer:\n1.Suma\n2.Resta\n3.Multipliacion\n4.Division\n"))
+    respuesta = 0
     if operacion.lower() == "suma" or int(operacion) == 1:
-        respuesta = n1 + n2
+        respuesta = sumar(n1,n2)
     elif operacion.lower() == "resta" or int(operacion) == 2:
-        respuesta = n1 - n2
+        respuesta = restar(n1, n2)
     elif operacion.lower() == "multiplicacion" or int(operacion) == 3:
-        respuesta = n1 * n2
+        respuesta = multiplicar(n1,n2)
     elif operacion.lower() == "division" or int(operacion) == 4:
-        respuesta = n1 / n2
+        respuesta = dividir(n1,n2)
     else:
         print("la opcion elegida no esta en la lista")
     print("la respuesta es {}".format(respuesta))
