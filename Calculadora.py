8dc19adabbd50167daa7e42c24dc3fa174466f9a
David Ariza <david.ariza@pi.edu.co>
2021-02-16 20:28:24 -0500

    se crea interfaz grafica

--- Calculadora.py
+++ Calculadora.py
@@ -1,21 +1,49 @@
-#se separan las funciones del core
-#from calculadoraFuncionesv3 import *
-#import funciones.calculadoraFuncionesv3
-import CalculadoraFunciones
-repetir = "si"
-while repetir.lower() == "si":
-    n1 = float(input("deme un numero\n"))
-    n2 = float(input("deme otro numero\n"))
-    operacion = str(input("que operacion desea hacer:\n1.Suma\n2.Resta\n3.Multipliacion\n4.Division\n"))
-    if operacion.lower == "suma" or int(operacion) == 1:
-        respuesta = funciones.CalculadoraFunciones.sumar(n1,n2)# no va a funcionar sino esta es una carpeta llamada funciones
-    elif operacion.lower == "resta" or int(operacion) == 2:
-        respuesta = CalculadoraFunciones.restar(n1, n2)
-    elif operacion.lower == "multiplicacion" or int(operacion) == 3:
-        respuesta = multiplicar(n1,n2)
-    elif operacion.lower == "division" or int(operacion) == 4:
-        respuesta = dividir(n1,n2)
-    else:
-        print("la opcion elegida no esta en la lista")
-    print("la respuesta es {}".format(respuesta))
-    repetir = str(input("desea hacer otra operacion\n"))
+from tkinter import  Tk, Entry, Grid, mainloop, Button, Label
+from tkinter.ttk import Combobox  # para agregar los de combobox
+
+def click():
+    #se deben poner aca dentro de la funcion para que no den error de conversion entre flat y text
+    v1 = n1.get()
+    v2 = n2.get()
+    if operaciones.get() == "+":
+        result.config(text=float(v1)+float(v2))
+    if operaciones.get() == "-":
+        result.config(text=float(v1)-float(v2))
+    if operaciones.get() == "x":
+        result.config(text=float(v1)*float(v2))
+    if operaciones.get() == u"\u00F7":
+        result.config(text=float(v1)/float(v2))
+    if operaciones.get() == "%":
+        result.config(text=float(v1)%float(v2))
+
+window = Tk()
+# titulo de la ventana
+window.title("calculadora v4")
+
+#titulo del frame
+label = Label(window, text="Esta es mi primera Calculadora")
+label.grid(columnspan=2, row=0)
+
+# entrada de numeros
+n1 = Entry(window, width=30)
+n2 = Entry(window, width=20)
+#ubicacion de numeros
+n1.grid(column=1, row=1)
+n2.grid(column=1, row=2)
+
+#eleccion de operacion
+operaciones = Combobox(window, width=5)
+operaciones["values"] = ("+", "-", "x",u"\u00F7","%")
+operaciones.current(0)
+#ubicacion de operaciones
+operaciones.grid(column=0, row=2)
+
+#resultados
+result = Label(window)
+result.grid(column=1, row=3)
+
+boton = Button(window, text="Resultado", command=click)
+boton.grid(column=0, row=3)
+
+#ciclo de la ventana
+window.mainloop()
